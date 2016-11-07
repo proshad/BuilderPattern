630aa9542125ee1248489a1fa51b20bce6c3a0ab
Ranada Proshad Biswas <proshad.biswas@tu.no>
2016-11-07 15:35:36 +0600

    First commit

---
+++ src/com/cefalo/school/builder/BrickHouseBuilder.java
@@ -0,0 +1,7 @@
+package com.cefalo.school.builder;
+
+/**
+ * Created by proshad on 11/7/16.
+ */
+public class BrickHouseBuilder {
+}
---
+++ src/com/cefalo/school/director/ThreeStroyBuildingDirector.java
@@ -0,0 +1,7 @@
+package com.cefalo.school.director;
+
+/**
+ * Created by proshad on 11/7/16.
+ */
+public class ThreeStroyBuildingDirector {
+}
